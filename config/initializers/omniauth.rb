0d28053c91cc74d3f1d1d55256151568088213a5
apache13 <apache13@gmail.com>
2014-05-14 14:10:27 +0700

    ignor config file

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  
-  provider :facebook, 'APP_ID', 'SECRET_KEY' , {:scope => 'email', :client_options => {:ssl => {:verify => false}}}
-  
-end
